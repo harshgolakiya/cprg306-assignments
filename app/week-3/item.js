2acf840f511e3792683a5d6e656561498b2d20fc
harshgolakiya <99644671+harshgolakiya@users.noreply.github.com>
2024-10-01 23:03:35 -0600

    week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -4,7 +4,7 @@ const Item = ({ name, quantity, category }) => {
     return (
       <li className="p-4 mb-2 border rounded-lg  bg-white">
         <div className="font-bold text-xl">{name}</div>
-        <div>Buy {quantity} in {category}</div>
+        <div>Buy {quantity} in {category} </div>
       </li>
     );
   };
