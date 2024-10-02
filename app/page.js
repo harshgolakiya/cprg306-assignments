5c59146b28f55c961cca64761b2265e26d7dc208
harshgolakiya <99644671+harshgolakiya@users.noreply.github.com>
2024-10-01 23:02:04 -0600

    init project

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function page(){
+  return(
+    <main>
+      <h1>Cprg-306 Assignments</h1>
+      <li><Link href="./week-2">Week2</Link></li>
+      <li><Link href="./week-3">Week3</Link></li>
+    </main>
+  )
+}
