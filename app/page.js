1e12cc99ee2a413faa803bc0fa23cefc310e9eb1
harshgolakiya <99644671+harshgolakiya@users.noreply.github.com>
2024-10-22 19:55:27 -0600

    week-6

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function page(){
       <li><Link href="./week-3">Week3</Link></li>
       <li><Link href="./week-4">Week4</Link></li>
       <li><Link href="./week-5">Week5</Link></li>
+      <li><Link href="./week-6">Week6</Link></li>
     </main>
   )
 }
